1eb5c4cc6fdd52f9e14cf763385edb24e5acee93
Austin White <AustinWhite93@gmail.com>
2019-02-06 22:58:38 -0800

    read_stdin

--- src/ssl.c
+++ src/ssl.c
@@ -24,11 +24,34 @@ char *itoa_base(int value, int base)
     return (ptr);
 }
 
+void    read_stdin(t_list **message)
+{
+    int ret;
+    char buf[READ_BUF_SIZE + 1];
+    char *str;
+    char *tmp;
+    t_list *new_message;
+
+    str = ft_strnew(1);
+    while ((ret = read(0, &buf, READ_BUF_SIZE)))
+    {
+        buf[ret] = '\0';
+        tmp = ft_strjoin(str, buf);
+        free(str);
+        str = tmp;
+    }
+    new_message = ft_lstnew(str, ft_strlen(str));
+    new_message->content_size = IS_STR;
+    ft_lstadd(message, new_message);
+}
+
 int main(int ac, char **av)
 {
     t_container contain;
 
     contain = parse_input(ac, av);
+    if (contain.message == NULL || contain.flags & FLG_P)
+        read_stdin(&contain.message);
     printf("hash_alg: %s\n", contain.hash_alg);
     printf("flags:    %04d\n          srqp\n", ft_atoi(itoa_base(contain.flags, 2)));
 
@@ -41,7 +64,5 @@ int main(int ac, char **av)
             printf("file:     %s\n", tmp->content);
         tmp = tmp->next;
     }
-    if (contain.message == NULL || contain.flags & FLG_P)
-        printf("read from stdin...\n");
     return (0);
 }
