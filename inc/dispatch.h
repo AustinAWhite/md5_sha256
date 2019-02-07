a1d75d5c634b87c4b900bc731379868e626cdd2a
Austin White <AustinWhite93@gmail.com>
2019-02-06 21:36:10 -0800

    added usage lookup

--- inc/dispatch.h
+++ inc/dispatch.h
@@ -8,11 +8,16 @@ static int  (*g_dispatch_funcs[])(t_container ssl) = {
     &md5,
     &sha256,
 };
-
+*/
 static const char   *g_dispatch_lookup[] = {
     "md5",
     "sha256",
     (char *)NULL,
 };
-*/
+
+static const char   *g_dispatch_usage[] = {
+    "usage: md5 [-pqrtx] [-s string] [files ...]",
+    "usage: sha256 [-pqrtx] [-s string] [files ...]",
+};
+
 #endif
