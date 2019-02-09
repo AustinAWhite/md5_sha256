ed48c0f1961348a1c92f8b70e76716e9c96a4ef4
Austin White <AustinWhite93@gmail.com>
2019-02-08 18:01:20 -0800

    removed itoa_base from ssl.c

--- src/ssl.c
+++ src/ssl.c
@@ -1,29 +1,5 @@
 #include "../inc/ssl.h"
 
-char *itoa_base(int value, int base)
-{
-    static  char rep[] = "0123456789abcdef";
-    static  char buf[50];
-    char    *ptr;
-    int     num;
-
-    ptr = &buf[49];
-    *ptr = '\0';
-    num = value;
-    if (value < 0 && base == 10)
-        value *= -1;
-    if (value == 0)
-        *--ptr = rep[value % base];
-    while (value != 0)
-    {
-        *--ptr = rep[value % base];
-        value /= base;
-    }
-    if (num < 0 && base == 10)
-        *--ptr = '-';
-    return (ptr);
-}
-
 void    read_stdin(t_list **message)
 {
     int ret;
