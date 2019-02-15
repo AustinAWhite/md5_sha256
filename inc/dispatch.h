e09e0f5bf0bfdcd788be998f9d74066f95a397a6
Austin White <AustinWhite93@gmail.com>
2019-02-14 20:50:31 -0800

    added sha256 to dispatch table

--- inc/dispatch.h
+++ inc/dispatch.h
@@ -6,18 +6,18 @@
 
 static void  (*g_dispatch_funcs[])(t_container ssl) = {
     &md5,
-    //&sha256,
+    &sha256,
 };
 
 static const char   *g_dispatch_lookup[] = {
     "md5",
-    //"sha256",
+    "sha256",
     (char *)NULL,
 };
 
 static const char   *g_dispatch_usage[] = {
     "usage: md5 [-pqrtx] [-s string] [files ...]",
-    //"usage: sha256 [-pqrtx] [-s string] [files ...]",
+    "usage: sha256 [-pqrtx] [-s string] [files ...]",
 };
 
 #endif
