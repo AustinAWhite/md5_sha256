2a8b3e0a4a3b951eed8578af7c2f8082511d44ec
Austin White <AustinWhite93@gmail.com>
2019-02-15 18:12:12 -0800

    sha_transform vars

--- src/sha256.c
+++ src/sha256.c
@@ -68,16 +68,17 @@ static int calc_chunk(uint8_t buffer[CHUNK_SIZE], sha256_ctx *ctx)
 void sha_transform(sha256_ctx *ctx, uint8_t hash[32], const void *input, size_t len)
 {
 	int i, j;
+	u_int32_t w_bufs[8];
+	const u_int8_t *ctxbuf_cpy; 
 	
 	while (calc_chunk(ctx->buffer, ctx)) {
-		uint32_t ah[8];
 		uint32_t w[64];
-		const uint8_t *p = ctx->buffer;
+		ctxbuf_cpy = ctx->buffer;
 		memset(w, 0x00, sizeof w);
 		for (i = 0; i < 16; i++) {
-			w[i] = (uint32_t) p[0] << 24 | (uint32_t) p[1] << 16 |
-				(uint32_t) p[2] << 8 | (uint32_t) p[3];
-			p += 4;
+			w[i] = (uint32_t) ctxbuf_cpy[0] << 24 | (uint32_t) ctxbuf_cpy[1] << 16 |
+							(uint32_t) ctxbuf_cpy[2] << 8 | (uint32_t) ctxbuf_cpy[3];
+			ctxbuf_cpy += 4;
 		}
 		for (i = 16; i < 64; i++) {
 			const uint32_t s0 = SHIFT_RIGHT(w[i - 15], 7) ^ SHIFT_RIGHT(w[i - 15], 18) ^ (w[i - 15] >> 3);
@@ -85,26 +86,26 @@ void sha_transform(sha256_ctx *ctx, uint8_t hash[32], const void *input, size_t 
 			w[i] = w[i - 16] + s0 + w[i - 7] + s1;
 		}
 		for (i = 0; i < 8; i++)
-			ah[i] = ctx->state[i];
+			w_bufs[i] = ctx->state[i];
 		for (i = 0; i < 64; i++) {
-			const uint32_t s1 = SHIFT_RIGHT(ah[4], 6) ^ SHIFT_RIGHT(ah[4], 11) ^ SHIFT_RIGHT(ah[4], 25);
-			const uint32_t ch = (ah[4] & ah[5]) ^ (~ah[4] & ah[6]);
-			const uint32_t temp1 = ah[7] + s1 + ch + sha256_k[i] + w[i];
-			const uint32_t s0 = SHIFT_RIGHT(ah[0], 2) ^ SHIFT_RIGHT(ah[0], 13) ^ SHIFT_RIGHT(ah[0], 22);
-			const uint32_t maj = (ah[0] & ah[1]) ^ (ah[0] & ah[2]) ^ (ah[1] & ah[2]);
+			const uint32_t s1 = SHIFT_RIGHT(w_bufs[4], 6) ^ SHIFT_RIGHT(w_bufs[4], 11) ^ SHIFT_RIGHT(w_bufs[4], 25);
+			const uint32_t ch = (w_bufs[4] & w_bufs[5]) ^ (~w_bufs[4] & w_bufs[6]);
+			const uint32_t temp1 = w_bufs[7] + s1 + ch + sha256_k[i] + w[i];
+			const uint32_t s0 = SHIFT_RIGHT(w_bufs[0], 2) ^ SHIFT_RIGHT(w_bufs[0], 13) ^ SHIFT_RIGHT(w_bufs[0], 22);
+			const uint32_t maj = (w_bufs[0] & w_bufs[1]) ^ (w_bufs[0] & w_bufs[2]) ^ (w_bufs[1] & w_bufs[2]);
 			const uint32_t temp2 = s0 + maj;
 
-			ah[7] = ah[6];
-			ah[6] = ah[5];
-			ah[5] = ah[4];
-			ah[4] = ah[3] + temp1;
-			ah[3] = ah[2];
-			ah[2] = ah[1];
-			ah[1] = ah[0];
-			ah[0] = temp1 + temp2;
+			w_bufs[7] = w_bufs[6];
+			w_bufs[6] = w_bufs[5];
+			w_bufs[5] = w_bufs[4];
+			w_bufs[4] = w_bufs[3] + temp1;
+			w_bufs[3] = w_bufs[2];
+			w_bufs[2] = w_bufs[1];
+			w_bufs[1] = w_bufs[0];
+			w_bufs[0] = temp1 + temp2;
 		}
 		for (i = 0; i < 8; i++)
-			ctx->state[i] += ah[i];
+			ctx->state[i] += w_bufs[i];
 	}
 	for (i = 0, j = 0; i < 8; i++)
 	{
