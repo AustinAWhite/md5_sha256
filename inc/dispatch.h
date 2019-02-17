416e860491b096c33b884349d8be902a22c98efd
Austin White <AustinWhite93@gmail.com>
2019-02-16 20:54:25 -0800

    expaned dispatch table

--- inc/dispatch.h
+++ inc/dispatch.h
@@ -8,17 +8,31 @@
 static void  (*dispatch_funcs[])(t_container ssl) = {
     &md5,
     &sha256,
+    //&sha224,
+    //&sha384,
+    //&sha512,
 };
 
 static const char   *dispatch_lookup[] = {
     "md5",
     "sha256",
+    //"sha224",
+    //"sha384",
+    //"sha512",
     (char *)NULL,
 };
 
+
+
+/*
+This is stupid get rid of this usage array
+*/
 static const char   *dispatch_usage[] = {
     "usage: md5 [-pqrtx] [-s string] [files ...]",
     "usage: sha256 [-pqrtx] [-s string] [files ...]",
+    //"usage: sha224 [-pqrtx] [-s string] [files ...]",
+    //"usage: sha384 [-pqrtx] [-s string] [files ...]",
+    //"usage: sha512 [-pqrtx] [-s string] [files ...]",
 };
 
 #endif
